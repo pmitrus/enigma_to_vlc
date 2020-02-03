4e336de92f698f8ed6e6abd09eeff15a89652309
pmitrus <mitrus@poczta.fm>
2020-02-04 00:51:20 +0100

    easier configuration

--- README.md
+++ README.md
@@ -4,23 +4,15 @@ script to download bouquet as vlc playlist
 
 sorry I'm learning python so code is for sure not optimal but creates for me working vlc playlist :)
 
-
 how to use?
 
-a) update by ip address of your device in all lines where you see http://192.168.0.11
+a) update by ip address of your device in all lines where you see http://192.168.0.11<br/>
+b) change userbouquet.dbe00.tv to userbouquet.dbe01.tv etc to iterate next bouquests<br/>
+c) update place to write playlist<br/>
 
-b) change userbouquet.dbe00.tv to userbouquet.dbe01.tv etc to iterate next bouquests
-
-url = """http://192.168.0.11/web/getservices?sRef=1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "userbouquet.dbe00.tv" ORDER BY bouquet"""
-
-and 
-
- f.write("\t\t\t<location>http://192.168.0.11:8001/"+programy['e2servicereference']+"</location>\r\n")
-
-
-update place to write playlist
-
-f= open("/home/pi/plajlista.xspf","w+")
+ip_addr="192.168.0.11"<br/>
+bouquet="userbouquet.dbe00.tv"<br/>
+fajl="/home/pi/plajlista.xspf"<br/>
 
 save and run
 
--- telewizja.py
+++ telewizja.py
@@ -2,10 +2,12 @@ import traceback
 import urllib3
 import xmltodict
 
+ip_addr="192.168.0.11"
+bouquet="userbouquet.dbe00.tv"
+fajl="/home/mitrus/plajlista.xspf"
+
 def getxml():
-    #update by ip address of your device
-    #change userbouquet.dbe00.tv to userbouquet.dbe01.tv etc to iterate next bouquests
-    url = """http://192.168.0.11/web/getservices?sRef=1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "userbouquet.dbe00.tv" ORDER BY bouquet"""
+    url = 'http://'+ip_addr+'/web/getservices?sRef=1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "'+bouquet+'" ORDER BY bouquet'
 
     http = urllib3.PoolManager()
 
@@ -18,8 +20,9 @@ def getxml():
 
 
 doc = getxml()
-#update place to write playlist
-f= open("/home/pi/plajlista.xspf","w+")
+print(doc)
+
+f= open(fajl,"w+")
 
 f.write("""<?xml version="1.0" encoding="UTF-8"?>\r\n""")
 f.write("""<playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/" version="1">\r\n""")
@@ -30,7 +33,7 @@ i=0
 
 for programy in doc['e2servicelist']['e2service']:
     f.write("""\t\t<track>\r\n""")
-    f.write("\t\t\t<location>http://192.168.0.11:8001/"+programy['e2servicereference']+"</location>\r\n")
+    f.write("\t\t\t<location>http://"+ip_addr+":8001/"+programy['e2servicereference']+"</location>\r\n")
     f.write("\t\t\t<title>"+programy['e2servicename']+"</title>\r\n")
     f.write("""\t\t\t<extension application="http://www.videolan.org/vlc/playlist/0">\r\n""")
     f.write("\t\t\t\t<vlc:id>"+str(i)+"</vlc:id>\r\n")
